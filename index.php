2d25843479b2205559d9077972208db3a8f12790
Tova0811 <91182586+Tova0811@users.noreply.github.com>
2022-01-14 12:05:06 +0100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="eng">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/thc.css">
-        <link rel="icon" href="../img/symbol.png" type="image" sizes="20x20">
-        <title>The Hidden Corner</title>
-    </head>
-    <body>
+<?php 
+include 'Incl/header.php';
+?>
         <div id="header">
             <a class="a-s" href="../index.html"><img class="symbol" src="../img/symbol.png" width="2000" height="2000" alt="pentagram" ></a>
             <h1>The Hidden Corner</h1>
@@ -34,14 +27,6 @@
             <img class="info-img" src="img/witch.png" width="564" height="664" alt="witch cauldron">
             <img class="tarot2" src="img/tarot.png" width="348" height="581" alt="tarot card six of rings">
         </div>
-                    
-     <footer>
-            <nav class="footer-nav">
-                <li><a href="html/blog.php">Blog</a></li>
-                <li><a href="html/gallery.php">Gallery</a></li>
-                <li><a href="html/about.php">About</a></li>
-                <li><a href="html/contact.php">Contact</a></li>
-            </nav>
-        </footer>
-    </body>
-</html>
+<?php                  
+include 'Incl/footer.php';
+?>
