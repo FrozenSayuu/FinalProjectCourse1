c712c47600a766f3468bf2f84a8a6b64fa4a3e03
Tova0811 <91182586+Tova0811@users.noreply.github.com>
2022-01-14 09:49:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-
-<html lang="eng">
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="css/thc.css">
-        <link rel="icon" href="img/symbol.png" type="image" sizes="20x20">
-        <title>The Hidden Corner</title>
-    </head>
-
-    <body>
+<?php
+include 'Incl/header.php';
+?>
         
         <div id="header">
         <a class="a-s" href="index.html"><img class="symbol" src="img/symbol.png" width="2000" height="2000" alt="pentagram" ></a>
