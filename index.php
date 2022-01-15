b417e87bc8e1c6b0ced26422fc24da74205a2fb5
Tova0811 <91182586+Tova0811@users.noreply.github.com>
2022-01-15 22:45:51 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<?php
-include 'Incl/header.php';
-?>
+<!DOCTYPE html>
+
+<html lang="eng">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/thc.css">
+        <link rel="icon" href="img/symbol.png" type="image" sizes="20x20">
+        <title>The Hidden Corner</title>
+    </head>
         
         <div id="header">
-        <a class="a-s" href="index.html"><img class="symbol" src="img/symbol.png" width="2000" height="2000" alt="pentagram" ></a>
+        <a class="a-s" href="index.php"><img class="symbol" src="img/symbol.png" width="2000" height="2000" alt="pentagram" ></a>
         <h1>The Hidden Corner</h1>
 
             <nav class="nav">
